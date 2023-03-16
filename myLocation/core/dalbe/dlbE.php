9617068aafb1f659876e9a16ef3f407fa516e371
The armors'll save us all <jl_beuzard@hotmail.com>
2023-03-16 01:12:03 +0000

    ! + minMod

---
+++ myLocation/core/dalbe/dlbE.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * data linearised by ntrpy -> (pdo/mysql)
+ * 
+ * zululad dream (origin:2007-2008@ISFCE) - march 2023
+ */
+class dlbE {
+
+    public function __construct($params) {
+
+    }
+
+
+
+
+}
---
+++ myLocation/core/mL_.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * myLocation core class
+ * 
+ * zululad - march 2023
+ * v0.0.1
+ */
+class mL_ {
+
+
+    public function __construct() {
+
+    }
+
+    
+
+
+}
