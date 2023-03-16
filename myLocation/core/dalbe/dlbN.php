030dae43b152cf3a0270bf4d9a765d197e684498
The armors'll save us all <jl_beuzard@hotmail.com>
2023-03-16 01:55:45 +0000

    ! nfos

--- myLocation/core/dalbe/dlbN.php
+++ myLocation/core/dalbe/dlbN.php
@@ -3,12 +3,14 @@
 /**
  * data linearised by known Ntropy where X is arbitrary revealed as a finite value :) -> (pdo/mysql)
  * 
- * zululad dream (origin:2007-2008@ISFCE) - march 2023
+ * zululad dream (origin:2007-2008@ISFCE -> 'data abstractions linearized by entropy' wechh lol) - march 2023
  */
 class dlbN {
 
     /**
      * crisp === draw :)
+     * in non lazy messy logic 'just a little effort to be free of infinite tasks in a env in move...
+     * take a draw, and then rest a bit :)
      */
     public function __construct($paramQuest) {
         // do this -- refactor this
