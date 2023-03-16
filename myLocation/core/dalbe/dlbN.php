533482e4de27fab7abfdcb72f4a442f43e7f3165
The armors'll save us all <jl_beuzard@hotmail.com>
2023-03-16 01:29:29 +0000

    ! minor mod

---
+++ myLocation/core/dalbe/dlbN.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * data linearised by known Ntropy where X is arbitrary revealed as a finite value :) -> (pdo/mysql)
+ * 
+ * zululad dream (origin:2007-2008@ISFCE) - march 2023
+ */
+class dlbN {
+
+    public function __construct($paramQuest) {
+        // do this -- refactor this
+        switch ( x = evaluate ($paramQuest) ) {
+
+            try {
+
+                case abs(x) > 799:
+                    x = crispMeThisNow(x);
+                    break;
+                default:
+                    doThisSearch($paramQuest);
+                    break;
+                
+            }
+        }
+    }
+
+    public function doThisSearch() {
+        // todo implement this
+    }
+
+
+
+}
